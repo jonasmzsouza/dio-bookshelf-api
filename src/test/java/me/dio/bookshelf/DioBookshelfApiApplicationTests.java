ab8ba4762f0e9dab04a865c66c2804d947c854d4
Jonas Souza <jonasmzsouza@gmail.com>
2023-10-21 17:28:56 -0300

    project created

---
+++ src/test/java/me/dio/bookshelf/DioBookshelfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.bookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioBookshelfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
